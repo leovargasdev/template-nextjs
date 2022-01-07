f2576269978a581447c6f65fe04edf5b18f3d963
Leonardo Vargas <leu1607@hotmail.com>
2022-01-07 08:54:37 -0300

    fix: melhorias de estilos globais e config de SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,16 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
 
           <link rel="icon" href="/favicon.ico" />
-          <meta name="author" content="Leonardo Luis de Vargas" />
 
+          <meta charSet="utf-8" />
           <meta property="og:type" content="website" />
           <meta property="og:locale" content="pt_BR" />
           <meta property="og:site_name" content="NextJS Boilerplate" />
+          <meta name="author" content="Leonardo Luis de Vargas" />
 
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
